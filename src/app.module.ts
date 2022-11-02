c8d9440da068d08cd47a6396a8eda034a7b10044
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-11-02 18:01:49 -0300

    database connection and setup envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.UTFPR_EVENTOS_DATABASE_PLUGIN,
+      host: process.env.UTFPR_EVENTOS_DATABASE_HOST,
+      port: process.env.UTFPR_EVENTOS_DATABASE_PORT,
+      username: process.env.UTFPR_EVENTOS_DATABASE_USER,
+      password: process.env.UTFPR_EVENTOS_DATABASE_PASSWORD,
+      database: process.env.UTFPR_EVENTOS_DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      synchronize: true,
+    } as TypeOrmModuleOptions),
+  ],
   controllers: [],
   providers: [],
 })

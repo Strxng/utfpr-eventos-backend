a2b87f3a1bb770cb855edccfbc84775122a320cf
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-11-02 18:07:58 -0300

    fix database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,15 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: process.env.UTFPR_EVENTOS_DATABASE_PLUGIN,
       host: process.env.UTFPR_EVENTOS_DATABASE_HOST,
-      port: process.env.UTFPR_EVENTOS_DATABASE_PORT,
+      port: parseInt(process.env.UTFPR_EVENTOS_DATABASE_PORT),
       username: process.env.UTFPR_EVENTOS_DATABASE_USER,
       password: process.env.UTFPR_EVENTOS_DATABASE_PASSWORD,
       database: process.env.UTFPR_EVENTOS_DATABASE_NAME,
       entities: [__dirname + '/**/*.entity{.js,.ts}'],
       synchronize: true,
+      options: {
+        encrypt: false,
+      },
     } as TypeOrmModuleOptions),
   ],
   controllers: [],
